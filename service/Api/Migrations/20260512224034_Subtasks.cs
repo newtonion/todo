965f2f2f2bb30e791bc6256aea8d38afddac7d51
Ben Newton <ben.d.newton@gmail.com>
2026-05-12 19:34:02 -0400

    Fix migration

--- service/Api/Migrations/20260512224034_Subtasks.cs
+++ service/Api/Migrations/20260512224034_Subtasks.cs
@@ -15,8 +15,7 @@ namespace Api.Migrations
                 name: "ParentListItemId",
                 table: "ListItems",
                 type: "TEXT",
-                nullable: false,
-                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+                nullable: true);
 
             migrationBuilder.CreateIndex(
                 name: "IX_ListItems_ParentListItemId",
